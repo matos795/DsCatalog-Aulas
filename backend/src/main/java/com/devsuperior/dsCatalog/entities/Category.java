3048794db2d17d5b29ece88ce5e723eff378b5aa
Alexandre Matos <matosalexandre795@gmail.com>
2025-11-29 16:14:19 -0300

    Update POM.XML

--- backend/src/main/java/com/devsuperior/dsCatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dsCatalog/entities/Category.java
@@ -1,7 +1,20 @@
 package com.devsuperior.dsCatalog.entities;
 
-public class Category {
+import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
