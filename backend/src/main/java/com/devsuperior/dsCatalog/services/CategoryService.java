3c0e0161a93ecfd81b459f31965e95ae31641847
Alexandre Matos <matosalexandre795@gmail.com>
2025-11-29 16:33:08 -0300

    New Service: CategoryService

---
+++ backend/src/main/java/com/devsuperior/dsCatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.dsCatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dsCatalog.entities.Category;
+import com.devsuperior.dsCatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+}
