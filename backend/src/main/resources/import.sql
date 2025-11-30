544a5ff2e2ffe5f9b0e71a4aea611505c02460a1
Alexandre Matos <matosalexandre795@gmail.com>
2025-11-29 22:43:24 -0300

    Seeding Database Update

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category VALUES('Books');
-INSERT INTO tb_category VALUES('Eletronics');
-INSERT INTO tb_category VALUES('Computers');
+INSERT INTO tb_category(name) VALUES('Books');
+INSERT INTO tb_category(name) VALUES('Eletronics');
+INSERT INTO tb_category(name) VALUES('Computers');
